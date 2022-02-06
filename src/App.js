622efd890ac43ff1ebeaae7c59e5f1e0d40db03f
Ross Keenan <ross.keenan@protonmail.com>
2022-02-06 17:11:12 -0600

    Created FeedbackItem, FeedbackList, and Card components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure that you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <h1>My App</h1>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
